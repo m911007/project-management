11e1e96a87fd6696a8d53b4bdebaa5cddb88f08a
m911007 <115946672+m911007@users.noreply.github.com>
2024-01-24 14:58:49 +0400

    24/1/2024  3:00 PM

---
+++ database/migrations/2024_01_24_103631_create_mentor_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentor_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->text('body');
+            $table->morphs('activable');
+            $table->unsignedBigInteger('mentor_id');
+            $table->foreign('mentor_id')->references('id')->on('users');
+            $table->boolean('confirm')->default(false);
+            $table->dateTime('confirm_date')->nullable();
+            $table->boolean('interested')->nullable();
+            $table->dateTime('interested_date')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mentor_comments');
+    }
+};
