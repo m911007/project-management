c4c1ce15c298982b605d6d203a31f61c3e3ee4cb
m911007 <115946672+m911007@users.noreply.github.com>
2024-01-24 09:36:09 +0400

    24/1/2024 9:30 AM

---
+++ database/migrations/2024_01_23_103633_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Department;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Department::class);
+            $table->string("title");
+            $table->text("description");
+            $table->string("image");
+            $table->boolean('is_published')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
